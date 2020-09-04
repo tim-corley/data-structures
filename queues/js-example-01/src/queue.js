cc266a9d7b33d2c88a2d75f1e1d8aaf34d7fe1ad
Tim Corley <tcorley215@gmail.com>
2020-09-04 09:24:01 -0400

    complete js queue example & (passing) tests

--- queues/js-example-01/src/queue.js
+++ queues/js-example-01/src/queue.js
@@ -3,11 +3,22 @@ export default class Queue {
     this.queue = [];
   }
 
+  // add item to front - "enqueue"
   add(item) {
     this.queue.unshift(item);
   }
-
+  // remove item from back - "dequeue"
   remove() {
-    this.queue.pop();
+    if (this.queue.length === 0) return "Queue is Empty!";
+    return this.queue.pop();
+  }
+  getFront() {
+    return this.queue[0];
+  }
+  getSize() {
+    return this.queue.length;
+  }
+  isEmpty() {
+    return this.queue.length === 0;
   }
 }
--- queues/js-example-01/src/queue.test.js
+++ queues/js-example-01/src/queue.test.js
@@ -5,3 +5,54 @@ test("item added to Queue", () => {
   demoQueue.add("Delta");
   expect(demoQueue.queue).toEqual(["Delta"]);
 });
+
+test("multiple items added to Queue", () => {
+  const demoQueue = new Queue();
+  demoQueue.add("Delta");
+  demoQueue.add("Echo");
+  demoQueue.add("Foxtrot");
+  expect(demoQueue.queue).toEqual(["Foxtrot", "Echo", "Delta"]);
+});
+
+test("item removed from Queue", () => {
+  const demoQueue = new Queue();
+  demoQueue.add("Alpha");
+  demoQueue.add("Bravo");
+  demoQueue.remove();
+  expect(demoQueue.queue).toEqual(["Bravo"]);
+});
+
+test("removing from empty Queue returns message", () => {
+  const demoQueue = new Queue();
+  demoQueue.add("Delta");
+  demoQueue.remove();
+  expect(demoQueue.remove()).toEqual("Queue is Empty!");
+});
+
+test("getFront returns first item in Queue", () => {
+  const demoQueue = new Queue();
+  demoQueue.add("Delta");
+  demoQueue.add("Echo");
+  expect(demoQueue.getFront()).toEqual("Echo");
+});
+
+test("getSize return correct Queue length", () => {
+  const demoQueue = new Queue();
+  demoQueue.add("Delta");
+  demoQueue.add("Echo");
+  demoQueue.add("Foxtrot");
+  expect(demoQueue.getSize()).toEqual(3);
+});
+
+test("empty check returns true on empty Queue", () => {
+  const demoQueue = new Queue();
+  demoQueue.add("Delta");
+  demoQueue.remove();
+  expect(demoQueue.isEmpty()).toBeTruthy();
+});
+
+test("empty check returns false on non-empty Queue", () => {
+  const demoQueue = new Queue();
+  demoQueue.add("Delta");
+  expect(demoQueue.isEmpty()).toBeFalsy();
+});
